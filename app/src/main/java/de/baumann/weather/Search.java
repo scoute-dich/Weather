17ab3b0c5894f1b64cdbbb2e7c768eddd8f8b4a5
scoute-dich <gaukler_faun@posteo.de>
2016-05-10 19:59:03 +0200

    code simplified

--- app/src/main/java/de/baumann/weather/Search.java
+++ app/src/main/java/de/baumann/weather/Search.java
@@ -286,73 +286,12 @@ public class Search extends AppCompatActivity implements NavigationView.OnNaviga
                                 startActivity(Intent.createChooser(sharingIntent, "Share using"));
                             }
                             if (options[item].equals(getString(R.string.action_share_screenshot))) {
-                                if (android.os.Build.VERSION.SDK_INT >= 23) {
-                                    int hasWRITE_EXTERNAL_STORAGE = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                                    if (hasWRITE_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED) {
-                                        if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                                            new AlertDialog.Builder(Search.this)
-                                                    .setMessage(R.string.permissions)
-                                                    .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
-                                                        @Override
-                                                        public void onClick(DialogInterface dialog, int which) {
-                                                            if (android.os.Build.VERSION.SDK_INT >= 23)
-                                                                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                                        REQUEST_CODE_ASK_PERMISSIONS);
-                                                        }
-                                                    })
-                                                    .setNegativeButton(getString(R.string.no), null)
-                                                    .show();
-                                            return;
-                                        }
-                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                REQUEST_CODE_ASK_PERMISSIONS);
-                                        return;
-                                    }
-                                }
 
-                                File directory = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/");
-                                if (!directory.exists()) {
-                                    directory.mkdirs();
-                                }
+                                screenshot();
 
+                                new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/");
                                 Date date = new Date();
                                 DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy_HH-mm", Locale.getDefault());
-
-                                String filename = getString(R.string.toast_screenshot) + " " + Environment.getExternalStorageDirectory() + "/Pictures/Websites/" + dateFormat.format(date) + ".jpg";
-                                Snackbar.make(swipeView, filename, Snackbar.LENGTH_LONG).show();
-
-                                mWebView.measure(View.MeasureSpec.makeMeasureSpec(
-                                        View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED),
-                                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-                                mWebView.layout(0, 0, mWebView.getMeasuredWidth(),
-                                        mWebView.getMeasuredHeight());
-                                mWebView.setDrawingCacheEnabled(true);
-                                mWebView.buildDrawingCache();
-                                Bitmap bm = Bitmap.createBitmap(mWebView.getMeasuredWidth(),
-                                        mWebView.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-
-                                Canvas bigcanvas = new Canvas(bm);
-                                Paint paint = new Paint();
-                                int iHeight = bm.getHeight();
-                                bigcanvas.drawBitmap(bm, 0, iHeight, paint);
-                                mWebView.draw(bigcanvas);
-                                System.out.println("1111111111111111111111="
-                                        + bigcanvas.getWidth());
-                                System.out.println("22222222222222222222222="
-                                        + bigcanvas.getHeight());
-
-                                try {
-                                    OutputStream fOut;
-                                    File file = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/", dateFormat.format(date) + ".jpg");
-                                    fOut = new FileOutputStream(file);
-
-                                    bm.compress(Bitmap.CompressFormat.PNG, 50, fOut);
-                                    fOut.flush();
-                                    fOut.close();
-                                    bm.recycle();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
 
                                 Intent sharingIntent = new Intent(Intent.ACTION_SEND);
                                 sharingIntent.setType("image/png");
@@ -362,87 +301,9 @@ public class Search extends AppCompatActivity implements NavigationView.OnNaviga
                                         + dateFormat.format(date) + ".jpg"));
                                 sharingIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
                                 startActivity(Intent.createChooser(sharingIntent, "Share using"));
-
-                                File file = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/"
-                                        + dateFormat.format(date) + ".jpg");
-                                Uri uri = Uri.fromFile(file);
-                                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
-                                sendBroadcast(intent);
                             }
                             if (options[item].equals(getString(R.string.action_save_screenshot))) {
-                                if (android.os.Build.VERSION.SDK_INT >= 23) {
-                                    int hasWRITE_EXTERNAL_STORAGE = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                                    if (hasWRITE_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED) {
-                                        if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                                            new AlertDialog.Builder(Search.this)
-                                                    .setMessage(R.string.permissions)
-                                                    .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
-                                                        @Override
-                                                        public void onClick(DialogInterface dialog, int which) {
-                                                            if (android.os.Build.VERSION.SDK_INT >= 23)
-                                                                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                                        REQUEST_CODE_ASK_PERMISSIONS);
-                                                        }
-                                                    })
-                                                    .setNegativeButton(getString(R.string.no), null)
-                                                    .show();
-                                            return;
-                                        }
-                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                REQUEST_CODE_ASK_PERMISSIONS);
-                                        return;
-                                    }
-                                }
-
-                                File directory = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/");
-                                if (!directory.exists()) {
-                                    directory.mkdirs();
-                                }
-
-                                Date date = new Date();
-                                DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy_HH-mm", Locale.getDefault());
-
-                                String filename = getString(R.string.toast_screenshot) + " " + Environment.getExternalStorageDirectory() + "/Pictures/Websites/" + dateFormat.format(date) + ".jpg";
-                                Snackbar.make(swipeView, filename, Snackbar.LENGTH_LONG).show();
-
-                                mWebView.measure(View.MeasureSpec.makeMeasureSpec(
-                                        View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED),
-                                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-                                mWebView.layout(0, 0, mWebView.getMeasuredWidth(),
-                                        mWebView.getMeasuredHeight());
-                                mWebView.setDrawingCacheEnabled(true);
-                                mWebView.buildDrawingCache();
-                                Bitmap bm = Bitmap.createBitmap(mWebView.getMeasuredWidth(),
-                                        mWebView.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-
-                                Canvas bigcanvas = new Canvas(bm);
-                                Paint paint = new Paint();
-                                int iHeight = bm.getHeight();
-                                bigcanvas.drawBitmap(bm, 0, iHeight, paint);
-                                mWebView.draw(bigcanvas);
-                                System.out.println("1111111111111111111111="
-                                        + bigcanvas.getWidth());
-                                System.out.println("22222222222222222222222="
-                                        + bigcanvas.getHeight());
-
-                                try {
-                                    OutputStream fOut;
-                                    File file = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/", dateFormat.format(date) + ".jpg");
-                                    fOut = new FileOutputStream(file);
-
-                                    bm.compress(Bitmap.CompressFormat.PNG, 50, fOut);
-                                    fOut.flush();
-                                    fOut.close();
-                                    bm.recycle();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-
-                                File file = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/"
-                                        + dateFormat.format(date) + ".jpg");
-                                Uri uri = Uri.fromFile(file);
-                                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
-                                sendBroadcast(intent);
+                                screenshot();
                             }
                         }
                     }).show();
@@ -571,5 +432,81 @@ public class Search extends AppCompatActivity implements NavigationView.OnNaviga
                     });
             dialog.show();
         }
+    }
+
+    private void screenshot() {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            int hasWRITE_EXTERNAL_STORAGE = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (hasWRITE_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED) {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    new AlertDialog.Builder(Search.this)
+                            .setMessage(R.string.permissions)
+                            .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (android.os.Build.VERSION.SDK_INT >= 23)
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                                REQUEST_CODE_ASK_PERMISSIONS);
+                                }
+                            })
+                            .setNegativeButton(getString(R.string.no), null)
+                            .show();
+                    return;
+                }
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return;
+            }
+        }
+
+        File directory = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/");
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy_HH-mm", Locale.getDefault());
+
+        String filename = getString(R.string.toast_screenshot) + " " + Environment.getExternalStorageDirectory() + "/Pictures/Websites/" + dateFormat.format(date) + ".jpg";
+        Snackbar.make(swipeView, filename, Snackbar.LENGTH_LONG).show();
+
+        mWebView.measure(View.MeasureSpec.makeMeasureSpec(
+                View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        mWebView.layout(0, 0, mWebView.getMeasuredWidth(),
+                mWebView.getMeasuredHeight());
+        mWebView.setDrawingCacheEnabled(true);
+        mWebView.buildDrawingCache();
+        Bitmap bm = Bitmap.createBitmap(mWebView.getMeasuredWidth(),
+                mWebView.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas bigcanvas = new Canvas(bm);
+        Paint paint = new Paint();
+        int iHeight = bm.getHeight();
+        bigcanvas.drawBitmap(bm, 0, iHeight, paint);
+        mWebView.draw(bigcanvas);
+        System.out.println("1111111111111111111111="
+                + bigcanvas.getWidth());
+        System.out.println("22222222222222222222222="
+                + bigcanvas.getHeight());
+
+        try {
+            OutputStream fOut;
+            File file = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/", dateFormat.format(date) + ".jpg");
+            fOut = new FileOutputStream(file);
+
+            bm.compress(Bitmap.CompressFormat.PNG, 50, fOut);
+            fOut.flush();
+            fOut.close();
+            bm.recycle();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        File file = new File(Environment.getExternalStorageDirectory() + "/Pictures/Websites/"
+                + dateFormat.format(date) + ".jpg");
+        Uri uri = Uri.fromFile(file);
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
+        sendBroadcast(intent);
     }
 }
