5867177aeb640965c218987186220cc31db503e8
scoute-dich <gaukler_faun@posteo.de>
2016-07-19 19:52:53 +0200

    appbar

--- app/src/main/java/de/baumann/weather/Browser.java
+++ app/src/main/java/de/baumann/weather/Browser.java
@@ -22,7 +22,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
