332c4de929ffd4bef785845534461eb534be940f
Max <maximilian.diez@student.kit.edu>
2017-03-08 01:10:10 +0100

    cleanup

--- app/src/main/java/de/baumann/weather/Browser.java
+++ app/src/main/java/de/baumann/weather/Browser.java
@@ -27,7 +27,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
